993088a35636866b05ca7fd372815ed919a2b4c4
Matt Sharp <matthew.sharp3@gmail.com>
2025-10-06 22:06:03 -0600

    fix: updated NewStore to return SQLstore docs: added inline docs

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -3,6 +3,7 @@ package db
 import "github.com/jackc/pgx/v5/pgxpool"
 
 // Interface that requires all methods from SQLC queries to be implemented
+// Use interface to allow for dependancy injection, instead of calling SQLStore directly. Pass value, not pointer.
 type Store interface {
 	Querier
 	// CreateSplitsTx eventually
@@ -11,13 +12,14 @@ type Store interface {
 
 // Implementation of the Store interface
 // Stores the connection pool and embeds SQL generated quries
+// Concrete implementation of the Store interface, connects to the database. Pass pointer.
 type SQLStore struct {
 	connPool *pgxpool.Pool // Pointer to connection pool
 	*Queries               // Embedding Queries to use SQLC methods
 }
 
-// Creates a new SQLStore which is an implementation of the Store interface
-func NewStore(connPool *pgxpool.Pool) (Store, error) {
+// Creates a new SQLStore
+func NewStore(connPool *pgxpool.Pool) (*SQLStore, error) {
 	// TODO return error on failure
 	return &SQLStore{
 		connPool: connPool,
